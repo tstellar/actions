978184a44e6ce3eaf00072f6531678230c4218ac
Tom Stellard <tstellar@redhat.com>
2020-04-03 18:36:30 +0000

    Add create-pull-request action

---
+++ create-pull-request/index.js
@@ -0,0 +1,23 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+  const token = core.getInput('token');
+  const octokit = new github.GitHub(token);
+
+  const owner = core.getInput('owner');
+  const repo = core.getInput('repo');
+  const title = core.getInput('title');
+  const head = core.getInput('head');
+  const base = core.getInput('base');
+
+  octokit.pulls.create({
+  owner,
+  repo,
+  title,
+  head,
+  base,
+});
+}
+
+run();
