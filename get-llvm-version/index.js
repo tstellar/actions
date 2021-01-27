5c65f578a9f55b5d6a3290dbcdd0a9c22784e1d6
Tom Stellard <tstellar@redhat.com>
2021-01-27 01:19:52 +0000

    UPDATE

--- get-llvm-version/index.js
+++ get-llvm-version/index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
-const { execSync } = require('child_process');
+const { exec } = require('child_process');
 const srcdir = core.getInput('srcdir');
-//const cmd = "grep -o 'LLVM_VERSION_\\(MAJOR\\|MINOR\\|PATCH\\) [0-9]\\+' " + srcdir + "/llvm/CMakeLists.txt"
-const cmd = "grep -o 'LLVM_VERSION_' " + srcdir + "/llvm/CMakeLists.txt"
+const cmd = "grep -o 'LLVM_VERSION_\\(MAJOR\\|MINOR\\|PATCH\\) [0-9]\\+' " + srcdir + "/llvm/CMakeLists.txt"
+//const cmd = "grep -o 'LLVM_VERSION_' " + srcdir + "/llvm/CMakeLists.txt"
 
 console.log(cmd);
-execSync(cmd, (error, stdout, stderr) => {
+exec(cmd, (error, stdout, stderr) => {
   console.log(`stdout: ${stdout}`);
   console.error(`stderr: ${stderr}`);
   if (error) {
