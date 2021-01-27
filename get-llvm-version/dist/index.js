422750f6fb4458b1a251f70ab1b94a1f04979bcb
Tom Stellard <tstellar@redhat.com>
2021-01-27 01:12:31 +0000

    update

--- get-llvm-version/dist/index.js
+++ get-llvm-version/dist/index.js
@@ -8,7 +8,8 @@ module.exports =
 const core = __nccwpck_require__(186);
 const { execSync } = __nccwpck_require__(129);
 const srcdir = core.getInput('srcdir');
-const cmd = "grep -o 'LLVM_VERSION_\\(MAJOR\\|MINOR\\|PATCH\\) [0-9]\\+' " + srcdir + "/llvm/CMakeLists.txt"
+//const cmd = "grep -o 'LLVM_VERSION_\\(MAJOR\\|MINOR\\|PATCH\\) [0-9]\\+' " + srcdir + "/llvm/CMakeLists.txt"
+const cmd = "grep -o 'LLVM_VERSION_' " + srcdir + "/llvm/CMakeLists.txt"
 
 console.log(cmd);
 execSync(cmd, (error, stdout, stderr) => {
@@ -19,7 +20,10 @@ execSync(cmd, (error, stdout, stderr) => {
     return;
   }
   stdout.split("\n").forEach(function(line) {
+    console.log(line);
     data = line.split(" ");
+    console.log(data[0])
+		  console.log(data[1])
     core.setOutput(data[0], data[1]);
   });
 });
--- get-llvm-version/index.js
+++ get-llvm-version/index.js
@@ -1,7 +1,8 @@
 const core = require('@actions/core');
 const { execSync } = require('child_process');
 const srcdir = core.getInput('srcdir');
-const cmd = "grep -o 'LLVM_VERSION_\\(MAJOR\\|MINOR\\|PATCH\\) [0-9]\\+' " + srcdir + "/llvm/CMakeLists.txt"
+//const cmd = "grep -o 'LLVM_VERSION_\\(MAJOR\\|MINOR\\|PATCH\\) [0-9]\\+' " + srcdir + "/llvm/CMakeLists.txt"
+const cmd = "grep -o 'LLVM_VERSION_' " + srcdir + "/llvm/CMakeLists.txt"
 
 console.log(cmd);
 execSync(cmd, (error, stdout, stderr) => {
@@ -12,7 +13,10 @@ execSync(cmd, (error, stdout, stderr) => {
     return;
   }
   stdout.split("\n").forEach(function(line) {
+    console.log(line);
     data = line.split(" ");
+    console.log(data[0])
+		  console.log(data[1])
     core.setOutput(data[0], data[1]);
   });
 });
